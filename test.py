acc7311fb4db071535a85128fe0e70a2fdf370ad
okunlade Jolade <jokunlade@gmail.com>
2022-05-24 08:21:53 +0100

    my initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+print("Addition")
+print(int(input("enter number ")) + int (input("enter number ")))
+
+print("Subtraction")
+print(int(input("enter number ")) - int (input("enter number ")))
+
+print("Multiplication")
+print(int(input("enter number ")) * int (input("enter number ")))
+
+print("Division")
+print(int(input("enter number ")) / int (input("enter number ")))
+
+print("Modulus")
+print(int(input("enter number ")) % int (input("enter number ")))
